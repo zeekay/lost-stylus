569774905c9be31c00a25b1715d98013d57898db
Zach Kelling <z@zeekay.io>
2015-09-28 10:32:39 -0700

    Remove cruft.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 module.exports = function() {
-  console.log('erp')
   return function(style) {
     style = this || style;
-    console.log('erp', style);
     style.import(__dirname + '/index.styl');
   }
 }
